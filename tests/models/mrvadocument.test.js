bacdce8c3ca426209a8418be63f4ed076557d5d3
Don Geronimo <don.geronimo@outlook.com>
2026-09-15 19:21:29 -0500

    Set properties explicitly when testing exemplary MRZ equality

--- tests/models/mrvadocument.test.js
+++ tests/models/mrvadocument.test.js
@@ -10,6 +10,16 @@ describe('TD3-sized Machine Readable Visa', () => {
   describe('Use properties to reconstruct exemplary MRZ from documentation [REF ICAO 9303 Part 7 Appendix A]', () => {
     it('should equal the exemplary MRVA in Appendix A-1 and A-2.', () => {
       const document = new MRVADocument({
+	typeCode: 'V',
+	authorityCode: 'UTO',
+	passportNumber: 'L8988901C',
+	birthDate: '1974-08-12',
+	genderMarker: 'F',
+	validThru: '1996-12-10',
+	nationalityCode: 'XXX',
+	primaryIdentifier: 'Eriksson',
+	secondaryIdentifier: 'Anna-Maria',
+	optionalData: '6ZE184226B',
         usePassportInMRZ: true
       });
       assert.deepStrictEqual(document.mrzLine1, 'V<UTOERIKSSON<<ANNA<MARIA<<<<<<<<<<<<<<<<<<<');
@@ -31,7 +41,8 @@ describe('TD3-sized Machine Readable Visa', () => {
       assert.deepStrictEqual(document.genderMarker, 'F');
       assert.deepStrictEqual(document.validThru, new Date('1996-12-10T00:00:00'));
       assert.deepStrictEqual(document.nationalityCode, 'XXX');
-      assert.deepStrictEqual(document.fullName, 'ERIKSSON, ANNA MARIA');
+      assert.deepStrictEqual(document.primaryIdentifier, 'ERIKSSON');
+      assert.deepStrictEqual(document.secondaryIdentifier, 'ANNA MARIA');
       assert.deepStrictEqual(document.optionalData, '6ZE184226B');
     });
   });
